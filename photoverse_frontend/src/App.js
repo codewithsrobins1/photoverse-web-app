c7932d5a4cc82a8ffe79963e7b8aa3113b502abb
unknown <codewithsrobins1@gmail.com>
2022-02-11 10:16:13 -0800

    login changes

--- photoverse_frontend/src/App.js
+++ photoverse_frontend/src/App.js
@@ -2,15 +2,15 @@ import React, { useEffect } from 'react';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 import Login from './components/Login';
 import Home from './container/Home';
+import { fetchUser } from './utils/fetchUser';
 
 const App = () => {
   const navigate = useNavigate();
   
   useEffect(() => {
-    const User = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear();
-
-    if (!User) navigate('/login');
-  }, []);
+    const user = fetchUser();
+    if(!user) navigate('/login');
+  }, [])
 
   return (
     <Routes>

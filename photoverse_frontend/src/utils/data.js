d3213c138589d7383ccb99edf0d08f893f33014c
unknown <codewithsrobins1@gmail.com>
2022-02-11 01:11:19 -0800

    final touches

--- photoverse_frontend/src/utils/data.js
+++ photoverse_frontend/src/utils/data.js
@@ -124,6 +124,56 @@ export const pinDetailMorePinQuery = (pin) => {
   return query;
 };
 
+export const userCreatedPinsQuery = (userId) => {
+  const query = `*[ _type == 'pin' && userId == '${userId}'] | order(_createdAt desc){
+    image{
+      asset->{
+        url
+      }
+    },
+    _id,
+    destination,
+    postedBy->{
+      _id,
+      userName,
+      image
+    },
+    save[]{
+      postedBy->{
+        _id,
+        userName,
+        image
+      },
+    },
+  }`;
+  return query;
+};
+
+export const userSavedPinsQuery = (userId) => {
+  const query = `*[_type == 'pin' && '${userId}' in save[].userId ] | order(_createdAt desc) {
+    image{
+      asset->{
+        url
+      }
+    },
+    _id,
+    destination,
+    postedBy->{
+      _id,
+      userName,
+      image
+    },
+    save[]{
+      postedBy->{
+        _id,
+        userName,
+        image
+      },
+    },
+  }`;
+  return query;
+};
+
 
 //Pin Categories
 export const categories = [
@@ -132,20 +182,24 @@ export const categories = [
       image: 'https://i.pinimg.com/750x/eb/47/44/eb4744eaa3b3ccd89749fa3470e2b0de.jpg',
     },
     {
+      name: 'technology',
+      image: 'https://inteng-storage.s3.amazonaws.com/img/iea/nR6bkXZxwo/sizes/software-engineering-skills_md.jpg',
+    },
+    {
+      name: 'NFTs',
+      image: 'https://cdn.vox-cdn.com/thumbor/ZkmdkuJUTLgJh96_FWQ5zweGGxo=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/23084330/bored_ape_nft_accidental_.jpg',
+    },
+    {
+      name: 'Video Games',
+      image: 'https://www.nme.com/wp-content/uploads/2021/04/league-of-legends-riot-games@2000x1270.jpg',
+    },
+    {
       name: 'fitness',
       image: 'https://i.pinimg.com/236x/25/14/29/251429345940a47490cc3d47dfe0a8eb.jpg',
     },
     {
       name: 'wallpaper',
       image: 'https://i.pinimg.com/236x/03/48/b6/0348b65919fcbe1e4f559dc4feb0ee13.jpg',
-    },
-    {
-      name: 'websites',
-      image: 'https://i.pinimg.com/750x/66/b1/29/66b1296d36598122e6a4c5452b5a7149.jpg',
-    },
-    {
-      name: 'photo',
-      image: 'https://i.pinimg.com/236x/72/8c/b4/728cb43f48ca762a75da645c121e5c57.jpg',
     },
     {
       name: 'food',
@@ -158,19 +212,10 @@ export const categories = [
     {
       name: 'art',
       image: 'https://i.pinimg.com/736x/f4/e5/ba/f4e5ba22311039662dd253be33bf5f0e.jpg',
-    }, {
+    }, 
+    {
       name: 'travel',
       image: 'https://i.pinimg.com/236x/fa/95/98/fa95986f2c408098531ca7cc78aee3a4.jpg',
-    },
-    {
-      name: 'quotes',
-      image: 'https://i.pinimg.com/236x/46/7c/17/467c17277badb00b638f8ec4da89a358.jpg',
-    }, {
-      name: 'cats',
-      image: 'https://i.pinimg.com/236x/6c/3c/52/6c3c529e8dadc7cffc4fddedd4caabe1.jpg',
-    }, {
-      name: 'dogs',
-      image: 'https://i.pinimg.com/236x/1b/c8/30/1bc83077e363db1a394bf6a64b071e9f.jpg',
     },
     {
       name: 'others',

6a76dfe8c3c4063d7cd84fd7feebc73aa5da1c04
unknown <codewithsrobins1@gmail.com>
2022-02-11 09:58:56 -0800

    removed unused variable

--- photoverse_frontend/src/App.js
+++ photoverse_frontend/src/App.js
@@ -2,7 +2,6 @@ import React, { useEffect } from 'react';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 import Login from './components/Login';
 import Home from './container/Home';
-import { fetchUser } from './utils/fetchUser';
 
 const App = () => {
   const navigate = useNavigate();
